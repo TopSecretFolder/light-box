78738f995496f71608868fbbb9847c006642267f
satyam <satyam.patel@rockbot.com>
2025-06-22 00:43:36 -0700

    update

--- provision/provision.sh
+++ provision/provision.sh
@@ -46,7 +46,7 @@ echo ""
 echo "update deps"
 sudo apt-get update
 echo "install some stuff"
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 echo "add docker keyring"
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -63,6 +63,6 @@ echo "apt get update"
 sudo apt-get update
 
 echo "install docker packages"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "docker install done"
