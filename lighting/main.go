6cf2fb0d00e97f77f23ebbb2762e89ea1b116bd9
satyam <satyam.patel@rockbot.com>
2025-06-22 02:45:14 -0700

    update

--- lighting/main.go
+++ lighting/main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+const VERSION = "v1.0.0"
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Light Box Server UPDATEDDD AGAIN")
+		return c.String(http.StatusOK, "Light Box Server: "+VERSION)
 	})
 	e.Logger.Fatal(e.Start(":80"))
 }
